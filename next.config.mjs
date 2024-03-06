7d0a58b178928a86c98ba747cb4a1996376a2ea4
Keitar6 <mateusz.statkiewicz@capgemini.com>
2024-03-06 18:59:00 +0100

    MF init

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,24 @@
+import { NextFederationPlugin } from '@module-federation/nextjs-mf';
+
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+
+  /**
+   * @param {import('webpack').Configuration} config
+   * @returns {import('webpack').Configuration}
+   */
+  webpack(config) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'next1',
+        filename: 'static/chunks/remoteEntry.js',
+      })
+    );
+
+    return config;
+  },
 };
 
 export default nextConfig;
